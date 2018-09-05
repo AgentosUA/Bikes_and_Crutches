e3367625748615e9bb622c31a5ab08828358177b
Agentos <agentoswork@gmail.com>
2018-09-05 19:27:17 +0300

    Added simple JS calculator

---
+++ calculator/js/index.js
@@ -0,0 +1,10 @@
+var bar = document.getElementById("calc-bar").value;
+
+
+document.getElementById("clear").addEventListener("click", function(event) {
+	document.getElementById("calc-bar").value = "";
+});
+
+function result () {
+
+}
